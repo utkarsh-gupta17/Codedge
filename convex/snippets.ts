ee6d850f00d65dba8cd951512dd11c04dd9eeb7b
utkarsh-gupta17 <211220061@nitdelhi.ac.in>
2024-12-19 01:03:53 +0530

    snippets working

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -71,33 +71,33 @@ export const deleteSnippet = mutation({
   },
 });
 
-// export const starSnippet = mutation({
-//   args: {
-//     snippetId: v.id("snippets"),
-//   },
-//   handler: async (ctx, args) => {
-//     const identity = await ctx.auth.getUserIdentity();
-//     if (!identity) throw new Error("Not authenticated");
+export const starSnippet = mutation({
+  args: {
+    snippetId: v.id("snippets"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Not authenticated");
 
-//     const existing = await ctx.db
-//       .query("stars")
-//       .withIndex("by_user_id_and_snippet_id")
-//       .filter(
-//         (q) =>
-//           q.eq(q.field("userId"), identity.subject) && q.eq(q.field("snippetId"), args.snippetId)
-//       )
-//       .first();
+    const existing = await ctx.db
+      .query("stars")
+      .withIndex("by_user_id_and_snippet_id")
+      .filter(
+        (q) =>
+          q.eq(q.field("userId"), identity.subject) && q.eq(q.field("snippetId"), args.snippetId)
+      )
+      .first();
 
-//     if (existing) {
-//       await ctx.db.delete(existing._id);
-//     } else {
-//       await ctx.db.insert("stars", {
-//         userId: identity.subject,
-//         snippetId: args.snippetId,
-//       });
-//     }
-//   },
-// });
+    if (existing) {
+      await ctx.db.delete(existing._id);
+    } else {
+      await ctx.db.insert("stars", {
+        userId: identity.subject,
+        snippetId: args.snippetId,
+      });
+    }
+  },
+});
 
 export const addComment = mutation({
   args: {
