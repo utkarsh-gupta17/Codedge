ab730016aa33220836972eb3794e8a882d5f8d67
utkarsh-gupta17 <211220061@nitdelhi.ac.in>
2024-12-20 11:26:07 +0530

    updated readme and signout button removed

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,21 +1,14 @@
-import { SignOutButton, SignUpButton } from "@clerk/nextjs";
 import EditorPanel from "./_components/EditorPanel";
 import Header from "./_components/Header";
 import OutputPanel from "./_components/OutputPanel";
-import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-
-  const user = await currentUser();
 
 
   return (
     <div className="min-h-screen">
       <div className="max-w-[1800px] mx-auto p-4">
         <Header />
-        {
-          user ? <SignOutButton/> : <SignUpButton/>
-        }
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           <EditorPanel />
           <OutputPanel />
